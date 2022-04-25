bdf585fadba78c8b34dbb498034854f976983182
ernisarahmaa <100423023+ernisarahmaa@users.noreply.github.com>
2022-04-26 00:18:55 +0700

    Add files via upload

---
+++ Tugas4/src/tugasjdbc/Connector.java
@@ -0,0 +1,21 @@
+package tugasjdbc;
+
+import java.sql.*;
+
+public class Connector {
+    String DBurl = "jdbc:mysql://localhost/tugasjdbc";
+    String DBusername = "root";
+    String DBpassword = "";
+    Connection koneksi;
+    Statement statement;
+    
+    public Connector() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");      
+            koneksi = (Connection) DriverManager.getConnection(DBurl,DBusername,DBpassword);
+            System.out.println("Koneksi Berhasil");
+        }catch(Exception ex){
+            System.out.println("Koneksi gagal");
+        }
+    }
+}
