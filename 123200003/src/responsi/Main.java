f52337f08b96066354e566911d5d9a18340129b5
ernisarahmaa <100423023+ernisarahmaa@users.noreply.github.com>
2022-05-24 12:43:58 +0700

    Add files via upload

---
+++ 123200003/src/responsi/Main.java
@@ -0,0 +1,10 @@
+package responsi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ViewMovie view = new ViewMovie();
+        ModelMovie model = new ModelMovie();
+        ControllerMovie controller = new ControllerMovie(model, view);
+    }
+}
